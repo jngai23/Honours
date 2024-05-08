457f08de87bacb83894017f006f7279d794f40f1
James Ngai <ngai.james23@gmail.com>
2024-05-08 19:39:01 +1000

    chembl api calls

--- Python/Target_Selection/Drug_Targets/drugbank_scraper.sh
+++ Python/Target_Selection/Drug_Targets/drugbank_scraper.sh
@@ -3,6 +3,5 @@
 #Output = Drug Targets
 #Scrapes Drugbank API to find all the targets of a list of SMILES
 
-curl 'https://www.ebi.ac.uk/chembl/target-predictions' -H 'Content-Type: application/json' --data-raw '{"smiles":"CS(C)=O"}' > temp.txt
+curl 'https://www.ebi.ac.uk/chembl/target-predictions' -H 'Content-Type: application/json' --data-raw '{"smiles":"Cc1ccc(Nc2nccc(N(C)c3ccc4c(C)n(C)nc4c3)n2)cc1S(N)(=O)=O"}' > temp.txt
 
-#python3 'targetextractor.py'
